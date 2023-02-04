87cb76b41d9ef746678dada3de400e9542d35ea8
yangss1981 <yangss1981@nate.com>
2023-02-04 17:54:06 +0900

    추가

---
+++ StockData/sise_market_sum.py
@@ -0,0 +1,22 @@
+# https://cocoabba.tistory.com/20
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://finance.naver.com/sise/sise_market_sum.nhn?page=1"
+
+# 페이지 데이터 조회
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'lxml')
+# print (soup)
+
+# 테이블 헤더 조회
+stock_head = soup.find("thead").find_all("th")
+data_head = [head.get_text() for head in stock_head]
+print(data_head)
+
+# 테이블 데이터 조회
+stock_list = soup.find("table", attrs={"class": "type_2"}).find("tbody").find_all("tr")
+for stock in stock_list:
+     if len(stock) > 1 :
+          print(stock.get_text().split())
