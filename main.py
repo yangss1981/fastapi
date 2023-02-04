b22efbd11524653ec5e11072ee2c5cf32077e103
yangss1981 <yangss1981@nate.com>
2023-02-04 16:27:25 +0900

    수정 테스트

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World !!!"}
+
 
 @app.get("/test/")
 async def test():
-    return {"message": "test"}
+    return {"message": "test !!!"}
